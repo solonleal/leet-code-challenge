0fcc97ef6151756fdccfd7bbde2d120973c0dac4
Solon Leal <soloncerqueira@gmail.com>
2024-07-08 17:39:17 -0300

    ajustando descrição dos testes do desafio 1 - twosum

--- src/test/java/br/nom/solonleal/leetcodechallenge/twosum/TwoSumTest.java
+++ src/test/java/br/nom/solonleal/leetcodechallenge/twosum/TwoSumTest.java
@@ -12,18 +12,18 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class TwoSumTest {
     TwoSum desafio = new TwoSum();
 
-    public static List<Arguments> cenariosTwoSum() {
-        Arguments cenario1 = Arguments.of("Opções [2,7,11,15] gerando soma 9", new int[]{2,7,11,15}, 9, new int[]{0,1});
-        Arguments cenario2 = Arguments.of("Opções [3,2,4] gerando soma 6", new int[]{3,2,4}, 6, new int[]{1,2});
-        Arguments cenario3 = Arguments.of("Opções [3,3] gerando soma 6", new int[]{3,3}, 6, new int[]{0,1});
+    private static List<Arguments> cenariosTwoSum() {
+        Arguments cenario1 = Arguments.of(new int[]{2,7,11,15}, 9, new int[]{0,1});
+        Arguments cenario2 = Arguments.of(new int[]{3,2,4}, 6, new int[]{1,2});
+        Arguments cenario3 = Arguments.of(new int[]{3,3}, 6, new int[]{0,1});
 
         return Arrays.asList(cenario1, cenario2, cenario3);
     }
 
     @SuppressWarnings("unused")
     @MethodSource("cenariosTwoSum")
-    @ParameterizedTest(name = "Cenário de Teste - {0}")
-    void testTwoSum(String cenario, int[] numeros, int valorObjetivo, int[] resultadoEsperado) {
+    @ParameterizedTest(name = "Cenário de Teste - entre os números {0}, quais os dois que somam {1} - os números das posições {2}")
+    void testTwoSum(int[] numeros, int valorObjetivo, int[] resultadoEsperado) {
         int[] retorno = desafio.twoSum(numeros, valorObjetivo);
 
         assertArrayEquals(resultadoEsperado, retorno);
