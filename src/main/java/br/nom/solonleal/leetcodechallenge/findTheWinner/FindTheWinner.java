06bf56a128bd1efd516363a6904efb571fdc69eb
Solon Leal <soloncerqueira@gmail.com>
2024-07-08 20:42:05 -0300

    refatorando utilizando o algoritmo para resolver o problema de josephus

--- src/main/java/br/nom/solonleal/leetcodechallenge/findTheWinner/FindTheWinner.java
+++ src/main/java/br/nom/solonleal/leetcodechallenge/findTheWinner/FindTheWinner.java
@@ -1,7 +1,5 @@
 package br.nom.solonleal.leetcodechallenge.findTheWinner;
 
-import java.util.Arrays;
-
 /**
  * Desafio 1823
  * url: <a href="https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/">...</a>
@@ -9,39 +7,10 @@ import java.util.Arrays;
 public class FindTheWinner {
 
     public int findTheWinner(int n, int k) {
-        int[] amigos = new int[n];
-        int qtdAmigosRestantes = n;
-        int amigoAtual = 0;
-
-        Arrays.fill(amigos, 1);
-
-        while(qtdAmigosRestantes > 1) {
-
-            for(int i = 0; i < k; i++) {
-                int amigo = amigos[amigoAtual];
-
-                while(amigo == 0) {
-                    amigoAtual = (amigoAtual == n - 1) ? 0 : amigoAtual + 1;
-                    amigo = amigos[amigoAtual];
-                }
-
-                if(i < k - 1) {
-                    amigoAtual = (amigoAtual == n - 1) ? 0 : amigoAtual + 1;
-                }
-            }
-
-            amigos[amigoAtual] = 0;
-            amigoAtual = (amigoAtual == n - 1) ? 0 : amigoAtual + 1;
-            qtdAmigosRestantes --;
+        int vencedor = 0;
+        for (int i = 1; i <= n; i++) {
+            vencedor = (vencedor + k) % i;
         }
-
-        for(int i = 0; i < n; i++) {
-            if(amigos[i] == 1) {
-                amigoAtual = i + 1;
-                break;
-            }
-        }
-
-        return amigoAtual;
+        return vencedor + 1;
     }
 }
