f210ebcbe9db6c4d200c23fa4805508e7d5443eb
Solon Leal <soloncerqueira@gmail.com>
2024-07-08 21:45:18 -0300

    desafio 1518 - water bottles

---
+++ src/main/java/br/nom/solonleal/leetcodechallenge/waterBottles/WaterBottles.java
@@ -0,0 +1,20 @@
+package br.nom.solonleal.leetcodechallenge.waterBottles;
+
+/*
+ * Desafio 1518
+ * url: <a href="https://leetcode.com/problems/water-bottles/description/">...</a>
+ */
+public class WaterBottles {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        if(numBottles < numExchange) {
+            return numBottles;
+        }
+
+        int quantidadeGarrafasVaziasTrocadas = numBottles / numExchange;
+        int quantidadeGarrafasRestantes = numBottles % numExchange;
+        int proximaInteracao = quantidadeGarrafasVaziasTrocadas + quantidadeGarrafasRestantes;
+
+        return numWaterBottles(proximaInteracao, numExchange) + numBottles - quantidadeGarrafasRestantes;
+    }
+}
---
+++ src/test/java/br/nom/solonleal/leetcodechallenge/waterBottles/WaterBottlesTest.java
@@ -0,0 +1,30 @@
+package br.nom.solonleal.leetcodechallenge.waterBottles;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WaterBottlesTest {
+    WaterBottles desafio = new WaterBottles();
+
+    private static List<Arguments> cenariosNumWaterBottles() {
+        Arguments cenario1 = Arguments.of(9, 3, 13);
+        Arguments cenario2 = Arguments.of(15, 4, 19);
+
+        return Arrays.asList(cenario1, cenario2);
+    }
+
+    @SuppressWarnings("unused")
+    @MethodSource("cenariosNumWaterBottles")
+    @ParameterizedTest(name = "Cenário de Teste - tendo {0} garrafas de água, podendo trocar {1} garrafas vazias por uma cheia, você consegue beber {2} de água")
+    void testTwoSum(int qtdGarrafas, int numGarrafasPorTroca, int resultadoEsperado) {
+        int retorno = desafio.numWaterBottles(qtdGarrafas, numGarrafasPorTroca);
+
+        assertEquals(resultadoEsperado, retorno);
+    }
+}
